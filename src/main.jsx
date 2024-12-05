efcb50994849d59c1528d0a043a9d610e46add65
Ranjan kashyap <kashyapranjan9977@gmail.com>
2024-12-05 11:25:14 +0530

    worker

--- src/main.jsx
+++ src/main.jsx
@@ -9,3 +9,16 @@ createRoot(document.getElementById("root")).render(
     <Toaster />
   </StrictMode>
 );
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./serviceworker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
